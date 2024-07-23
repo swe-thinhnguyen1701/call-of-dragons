ea57366e7b86ea30baad92edff2add7d025657a7
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-23 18:15:23 -0500

    build greeting section and fix nav bar

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 const menu = $("#menu");
+const video = $("#video");
+const logo = $("#logo");
 
 const menuToggleBtn = $(".menu-toggle-btn");
 
@@ -11,7 +13,16 @@ const defaultMenu = () => {
     menu.removeAttr("style");
 }
 
+const videoResize = () => {
+  let width = $(window).width();
+  video.attr("width", width);
+  video.attr("height", width / 1.775);
+  logo.height(width / 1.775);
+}
+
 $(document).ready(() => {
   menuToggleBtn.on("click", menuToggleHandler);
   $(window).on("resize", defaultMenu);
+  $(window).on("resize", videoResize);
+  videoResize();
 });
